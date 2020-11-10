e6a6e012ecd4f01b243efa335c474aa712f3145b
Davidxy9 <davidwilliamlima@hotmail.com>
2020-11-10 12:12:07 -0300

    route post fix and CreateService finish with validate

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,10 @@
-// import AppError from '../errors/AppError';
-
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm'; // USAR REP/ USAR MODEL
+import AppError from '../errors/AppError';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
 interface Request {
   title: string;
@@ -21,11 +21,36 @@ class CreateTransactionService {
     category,
   }: Request): Promise<Transaction> {
     const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryepository = getRepository(Category); // criar um repositório através da model
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type == 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    // Verificar se a categoria já existe
+    let transactionCategory = await categoryepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+    // Não existe? Crie ela
+    if (!transactionCategory) {
+      transactionCategory = categoryepository.create({
+        title: category,
+      });
+
+      await categoryepository.save(transactionCategory);
+    }
+
+    // Existe? Buscar ela no banco de dados e usar o id que foi retornado
 
     const transaction = transactionsRepository.create({
       title,
       value,
       type,
+      category: transactionCategory,
     });
 
     await transactionsRepository.save(transaction);
