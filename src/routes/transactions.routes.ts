d2b035514f40565b1eae0ad1e5373bb6b589c570
Davidxy9 <davidwilliamlima@hotmail.com>
2020-11-10 09:07:10 -0300

    route post and CreateTransService add

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,19 +1,28 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
-transactionsRouter.get('/', async (request, response) => {
-  const { title, value, type, category } = request.body;
-
-});
+transactionsRouter.get('/', async (request, response) => { });
 
 transactionsRouter.post('/', async (request, response) => {
+  const { title, value, type, category } = request.body;
 
+  const createTransaction = new CreateTransactionService(); // instanciar o service para dentro da rota
+
+  // executar o service
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
